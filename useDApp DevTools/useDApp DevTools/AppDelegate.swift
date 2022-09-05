64aacbd7c573ec1ce927ceccca01d6a7980a4f82
sam bacha <sam@manifoldfinance.com>
2022-09-05 06:52:02 -0700

    safari

---
+++ useDApp DevTools/useDApp DevTools/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  useDApp DevTools
+//
+//  Created by Sam Bacha on 9/5/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
